577c0b16adc9eba4f79431d17f7ccf8b14b5ce96
MurkyGlue <vp654848@gmail.com>
2024-08-02 01:15:39 +0300

    sqrt started

---
+++ sqrt/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace sqrt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b = Convert.ToDouble(Console.ReadLine()); //49
+            int c = Convert.ToInt32(Console.ReadLine()); //2
+            double s = 1.0;
+            double i = 0;
+            string r = "";
+
+            for(int v = 0; v < 10; v++)
+            {
+                if (Math.Pow(i, c) < b)
+                {
+                    i += s;
+                }
+                else if (Math.Pow(i, c) == b)
+                {
+                    r += i;
+                    break;
+                }
+                else
+                {
+                    i -= s;
+                    r += i;
+                    s /= 10;
+                }
+            }
+
+            Console.WriteLine(r);
+
+        }
+    }
+}
