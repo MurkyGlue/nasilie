eb2d7e2b259a75e51cda300648aeed2edc19e652
MurkyGlue <vp654848@gmail.com>
2024-08-05 21:56:52 +0300

    добавлены поставленные блоки

--- flyjet.py
+++ flyjet.py
@@ -15,10 +15,14 @@ blockSize1 = baseSize[0]
 block2 = [-baseSize[0], height-2*baseSize[1]-200]
 blockSize2 = baseSize[0]
 
+blocks = []
+sizes = []
+
 
 screencol = (100, 100, 100)
 blockcol1 = (255, 0, 0)
 blockcol2 = (0, 255, 0)
+blockcol3 = (0, 0, 255)
 
 blockSpeed = 0.5
 
@@ -44,6 +48,8 @@ def step():
 	base[1] += baseSize[1]
 	block1[1] += baseSize[1]
 	block2[1] += baseSize[1]
+	for i in range(len(blocks)):
+		blocks[i][1] += baseSize[1]
 
 def cut1():
 	global blockSize1, blockSize2, block1, block2, b
@@ -52,13 +58,13 @@ def cut1():
 		if block1[0] < base[0]:
 			blockSize1 -= base[0] - block1[0]
 			block1[0] = base[0]
-		elif block1[0]+blockSize1 > base[0]+baseSize[0]:
+		if block1[0]+blockSize1 > base[0]+baseSize[0]:
 			blockSize1 += (base[0]+baseSize[0]) - (block1[0]+blockSize1)
 	else:
 		if block1[0] < block2[0]:
 			blockSize1 -= block2[0] - block1[0]
 			block1[0] = block2[0]
-		elif block1[0]+blockSize1 > block2[0]+blockSize2:									#оно должно работать, но не работает
+		if block1[0]+blockSize1 > block2[0]+blockSize2:									#оно должно работать, но не работает
 			blockSize1 += (block2[0]+blockSize2) - (block1[0]+blockSize1)
 	
 def cut2():
@@ -67,7 +73,7 @@ def cut2():
 	if block2[0] < block1[0]:
 		blockSize2 -= block1[0] - block2[0]
 		block2[0] = block1[0]
-	elif block2[0]+blockSize2 > block1[0]+blockSize1:
+	if block2[0]+blockSize2 > block1[0]+blockSize1:
 		blockSize2 += (block1[0]+blockSize1) - (block2[0]+blockSize2)
 
 
@@ -79,6 +85,8 @@ while True:
 	pygame.draw.rect(screen, blockcol2, (base, baseSize))
 	pygame.draw.rect(screen, blockcol1, (block1, [blockSize1, baseSize[1]]))
 	pygame.draw.rect(screen, blockcol2, (block2, [blockSize2, baseSize[1]]))
+	for i in range(len(blocks)):
+		pygame.draw.rect(screen, blockcol3, (blocks[i], [sizes[i], baseSize[1]]))
 	
 	pygame.display.flip()
 	
@@ -93,9 +101,18 @@ while True:
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_SPACE:
 				switch = not switch
-				if switch: block1[1] -= 2*baseSize[1]; cut2(); back1()
-				else: block2[1] -= 2*baseSize[1]; cut1(); back2()
+				if switch:
+					blocks.append(block1)
+					sizes.append(blockSize1)
+					cut2()
+					back1()
+				else:
+					blocks.append(block2)
+					sizes.append(blockSize2)
+					cut1()
+					back2()
 				step()
+				#if block1[0] < block2[0] and 
 
 				
 
