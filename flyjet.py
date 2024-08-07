9c4712160b899afd3d4a1dada9341fd1edaa326b
MurkyGlue <vp654848@gmail.com>
2024-08-07 04:40:12 +0300

    добавлена анимация смерти

--- flyjet.py
+++ flyjet.py
@@ -27,7 +27,9 @@ blockcol3 = (0, 0, 255)
 blockSpeed = 0.5
 
 switch = True
+alive = True
 b = True
+c = False
 
 
 def move1():
@@ -75,7 +77,22 @@ def cut2():
 		block2[0] = block1[0]
 	if block2[0]+blockSize2 > block1[0]+blockSize1:
 		blockSize2 += (block1[0]+blockSize1) - (block2[0]+blockSize2)
-
+	
+def dead():
+	global blockSpeed, alive
+	blockSpeed = 0
+	alive = False
+	while base[1] > height-baseSize[1]-200:
+		screen.fill(screencol)
+		pygame.draw.rect(screen, blockcol2, (base, baseSize))
+		pygame.draw.rect(screen, blockcol1, (block1, [blockSize1, baseSize[1]]))
+		pygame.draw.rect(screen, blockcol2, (block2, [blockSize2, baseSize[1]]))
+		for i in range(len(blocks)):
+			blocks[i][1] -= 0.3
+			pygame.draw.rect(screen, blockcol3, (blocks[i], [sizes[i], baseSize[1]]))
+		pygame.display.flip()
+		base[1] -= 0.3
+	pygame.quit()
 
 
 
@@ -87,32 +104,33 @@ while True:
 	pygame.draw.rect(screen, blockcol2, (block2, [blockSize2, baseSize[1]]))
 	for i in range(len(blocks)):
 		pygame.draw.rect(screen, blockcol3, (blocks[i], [sizes[i], baseSize[1]]))
-	
 	pygame.display.flip()
 	
-	if switch:
-		move1()
-	else:
-		move2()
+	if switch: move1()
+	else: move2()
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			pygame.quit()
 		if event.type == pygame.KEYDOWN:
 			if event.key == pygame.K_SPACE:
-				switch = not switch
-				if switch:
-					blocks.append(block1)
-					sizes.append(blockSize1)
-					cut2()
-					back1()
-				else:
-					blocks.append(block2)
-					sizes.append(blockSize2)
-					cut1()
-					back2()
-				step()
-				#if block1[0] < block2[0] and 
+				if alive:
+					if c:
+						if block2[0]+blockSize2 < block1[0] or block2[0] > block1[0]+blockSize1: dead()
+					else: c = True	
+					switch = not switch
+					if switch:
+						blocks.append(block1)
+						sizes.append(blockSize1)
+						cut2()
+						back1()
+					else:
+						blocks.append(block2)
+						sizes.append(blockSize2)
+						cut1()
+						back2()
+					step()
+					
 
 				
 
