a54ff06edee7d4d7ca874f77e7412d15d62b5abd
MurkyGlue <vp654848@gmail.com>
2024-10-12 13:14:04 +0300

    поменял разрешение

--- rasp10.py
+++ rasp10.py
@@ -1,6 +1,6 @@
 import time, pygame
 pygame.font.init()
-sc = pygame.display.set_mode((720, 1280))
+sc = pygame.display.set_mode((1080, 2340))
 
 lessons = [
     ["Разговоры о важном 105", "Английский 310", "Русский язык 301", "Алгебра 303", "Геометрия 303", "История 308", "Физ-ра", "Литература 113"],
@@ -16,15 +16,15 @@ bells = [
     [8.45, 9.35, 10.30, 11.25, 12.25, 13.25, 14.15, 15.05, 15.55]
 ]
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
-pos = [0, 80, 160, 240, 320, 400, 480, 560]
-les = [pygame.font.SysFont("Adobe Gothic Std Kalin", 50), 
-       pygame.font.SysFont("Adobe Gothic Std Kalin", 50), 
-       pygame.font.SysFont("Adobe Gothic Std Kalin", 50), 
-       pygame.font.SysFont("Adobe Gothic Std Kalin", 50), 
-       pygame.font.SysFont("Adobe Gothic Std Kalin", 50), 
-       pygame.font.SysFont("Adobe Gothic Std Kalin", 50), 
-       pygame.font.SysFont("Adobe Gothic Std Kalin", 50), 
-       pygame.font.SysFont("Adobe Gothic Std Kalin", 50)]
+pos = [0, 150, 300, 450, 600, 750, 900, 1050]
+les = [pygame.font.SysFont("Adobe Gothic Std Kalin", 100), 
+       pygame.font.SysFont("Adobe Gothic Std Kalin", 100), 
+       pygame.font.SysFont("Adobe Gothic Std Kalin", 100), 
+       pygame.font.SysFont("Adobe Gothic Std Kalin", 100), 
+       pygame.font.SysFont("Adobe Gothic Std Kalin", 100), 
+       pygame.font.SysFont("Adobe Gothic Std Kalin", 100), 
+       pygame.font.SysFont("Adobe Gothic Std Kalin", 100), 
+       pygame.font.SysFont("Adobe Gothic Std Kalin", 100)]
 c = 10
 pygame.init()
 while True:
@@ -32,12 +32,17 @@ while True:
     t = time.asctime(time.localtime(time.time()))
     hm = int(t[11:13]) + float(t[14:16])/100
     day = days.index(t[0:3])
+    day = 0
+    hm = 8.15
     for i in range(len(bells[0])):
         if bells[0][i] <= hm <= bells[1][i]:check = True;break
         else:check = False
     for i in range(len(lessons[day])):
         if hm <= bells[1][i]:c = i;break
     for i in range(len(lessons[day])-c):
-        if check:pygame.draw.rect(sc, (0, 255, 0), [0, 0, 720, 80]);check = False
-        sc.blit(les[i].render(lessons[day][i+c], True, (0, 0, 0)), [40, pos[i]+20])
+        if check:pygame.draw.rect(sc, (0, 255, 0), [0, 0, 1080, 150]);check = False
+        sc.blit(les[i].render(lessons[day][i+c], True, (0, 0, 0)), [80, pos[i]+40])
     pygame.display.flip()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
